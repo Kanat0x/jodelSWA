44f7aa011c6cceede28ed7e54e0ff50963a9dace
Kanat0x <senor.sintorax@gmail.com>
2022-01-10 20:02:00 +0100

    fixed login bug and added votingDao and votingDaoTest

--- Jodel/src/main/resources/import.sql
+++ Jodel/src/main/resources/import.sql
@@ -12,3 +12,12 @@ INSERT INTO Comment (id, latitude, longitude, postedat, text, authorId, postId)
        VALUES ("3", "123", "123", "2022.09.01", "1. Test Comment User 2", "2", "3");
 INSERT INTO Comment (id, latitude, longitude, postedat, text, authorId, postId)
        VALUES ("4", "123", "123", "2022.09.01", "2. Test Comment User 2", "2", "4");
+
+INSERT INTO Post (id, latitude, longitude, postedat, text, authorId)
+      VALUES ("1", "123", "123", "2022.09.01", "Das ist ein Post", "1");
+INSERT INTO Post (id, latitude, longitude, postedat, text, authorId)
+      VALUES ("2", "123", "123", "2022.09.01", "Und das ist der zweite Post", "2");
+INSERT INTO Post (id, latitude, longitude, postedat, text, authorId)
+      VALUES ("3", "123", "123", "2020.09.01", "Ich habe keine lust mehr", "1");
+INSERT INTO Post (id, latitude, longitude, postedat, text, authorId)
+      VALUES ("4", "123", "123", "2020.09.01", "Das ist eaber schönes wetter", "2");
